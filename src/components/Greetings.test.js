6368e7c245b970156ed2657999300b691e4f71b0
Nikhil <nikhilsamuel1404@gmail.com>
2021-12-09 07:21:47 -0500

    Grouping tests together with a test suit

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,10 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Greetings";
-test("renders hello world as a test", () => {
-  //Arrange
-  render(<Greetings />);
-  //Act
-  //Assert
-  const helloWorldElement = screen.getByText("Hello World"); //set exact : false if you dont want an exact match
-  expect(helloWorldElement).toBeInTheDocument();
+
+describe("Greetin Component", () => {
+  //describe is used to create a test suite for a particular component or t group diff tests together.
+  test("renders hello world as a test", () => {
+    //Arrange
+    render(<Greetings />);
+    //Act
+    //Assert
+    const helloWorldElement = screen.getByText("Hello World"); //set exact : false if you dont want an exact match
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
